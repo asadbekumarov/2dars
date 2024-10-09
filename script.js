ec772dc1b9e2bf73bb1916be946e8494fe606b70
asadbekumarov <asadbekumarov922@gmail.com>
2024-10-10 00:01:23 +0500

    end

---
+++ script.js
@@ -0,0 +1,139 @@
+// // ------------------------------------------------------------------
+// 1-topshiriq
+// ---------------------------------------------------------------------
+// let a = parseFloat(prompt("Son kiriting"));
+// let b = parseFloat(prompt("Yana bitta son kiriting"));
+// let c = prompt("Operator kiriting (+, -, *, /)");
+// if (c === "+") {
+//   console.log(a + b);
+// } else if (c === "-") {
+//   console.log(a - b);
+// } else if (c === "*") {
+//   console.log(a * b);
+// } else if (c === "/") {
+//   console.log(a / b);
+// } else {
+//   console.log("Noto'g'ri qiymat kiritildi");
+// }
+// ---------------------------------------------------------------------
+// 2-topshiriq
+// ---------------------------------------------------------------------
+// let name = prompt("ismingizni kirting");
+// let family = prompt("familyangizni kiriting");
+// let age = prompt("Yoshingizni kirting");
+// let address = prompt("Manzilingizni kirting");
+// let hobby = prompt("Yoqtirgan mashg'lotingizni kiriting");
+// let occupation = prompt("Kasbingizni kiriting");
+// let phoneNumber = prompt("Telfon raqamingizni kiriting");
+// let yourHeight = prompt("Bo'yingizni kiriting");
+// let weight = prompt("Vazningizni kiriting");
+// let theBestCity = prompt("Siz uchun eng zo'r shahar qaysi");
+// let user = {
+//   name: name,
+//   family: family,
+//   age: age,
+//   address: address,
+//   hobby: hobby,
+//   occupation: occupation,
+//   phoneNumber: phoneNumber,
+//   yourHeight: yourHeight,
+//   weight: weight,
+//   theBestCity: theBestCity,
+// };
+// console.log(user);
+// ---------------------------------------------------------------------
+// 3-topshiriq
+// // ------------------------------------------------------------------
+// let bozor = confirm("bozorlikni hisoplaymiz");
+// let behi = +prompt("behini narxini kiriting");
+// let olma = +prompt("olma narxini kiriting");
+// let olcha = +prompt("olcha narxini kiriting");
+// let nok = +prompt("nok narxini kiriting");
+// let anor = +prompt("anor narxini kiriting");
+// let sum = behi + olma + olcha + nok + anor;
+// console.log("hamma praduqtalarning narxi " + sum);
+// ---------------------------------------------------------------------
+// 4-topshiriq
+// ---------------------------------------------------------------------
+// let fizika = +prompt("fizika hisoplaymiz");
+// let matem = +prompt("matem narxini kiriting");
+// let biyolagiya = +prompt("biyolagiya narxini kiriting");
+// let tarix = +prompt("tarix narxini kiriting");
+// let kimya = +prompt("kimya narxini kiriting");
+// let sum = fizika + matem + biyolagiya + tarix + kimya;
+// console.log("sizning o'rtacha bahongiz " + sum / 5);
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// QO'SHIMCHA.
+// 1. a nomli o'zgaruvchi e'lon qiling unga 10 raqam qiymat biriktiring, b degan o'zgaruvchi oching unga 5 ni biriktiring va console ga sonlarni yig'indisini chiqaring.
+// ---------------------------------------------------------------------
+// let a = 10;
+// let b = 5;
+// console.log(a + b);
+
+// 2. a nomli o'zgaruvchi e'lon qiling unga 25 raqam qiymat biriktiring, b degan
+// o'zgaruvchi oching unga 15 ni biriktiring va natijani result degan o'zgaruchiga oling (a dan b ni ayring), consolega result ni chiqaring.
+// ---------------------------------------------------------------------
+// let a = 25;
+// let b = 15;
+// let sum = a - b;
+// console.log(sum);
+
+// 3. a nomli o'zgaruvchi e'lon qiling unga 10 ni biriktiring b nomli o'zgaruvchi e'lon qiling va unga 5 ni biriktiring keyin a dan b ni ayring natijani c nomli o'zgaruvchiga chiqaring.
+// keyin d nomli o'zgaruvchi e'lon qilib unga 7 ni biriktiring c va d ni ko'paytiring va natijani result nomli o'zgaruvchida console ga chiqaring.
+// ---------------------------------------------------------------------
+// let a = 10;
+// let b = 5;
+// let c = 10 - 5;
+// let d = 7;
+// let sum = c * d;
+// console.log(sum);
+
+// 4. a nomli o'zgaruvchi e'lon qiling unga 10 ni biriktiring b o'zgaruvchiga 3 ni biriktiring va console ga a dan b ni boʻlgandagi qoldig'ini chiqaring.
+// ---------------------------------------------------------------------
+// let a = 10;
+// let b = 3;
+// console.log(a % b);
+
+// 5. 2 ning 10 chi darajasini console ga chiqaring
+// ---------------------------------------------------------------------
+// let a = 2;
+// console.log(a ^ 10);
+
+// 6.Kvadratning tomoni a berilgan. Uning perimetri aniqlansin. P=4-a.
+// ---------------------------------------------------------------------
+// let a = 5;
+// let p = 4 * a;
+// console.log(p);
+
+// 7.Kvadratning tomoni a berilgan. Uning yuzasi aniqlansin. S = a2.
+// ---------------------------------------------------------------------
+// let a = 5;
+// let y = a * a;
+// console.log(y);
+
+// 8.To`g`ri to`rtburchakning tomonlari a va b berilgan. Uning yuzasi S = a * b; va P = 2 * (a + b) perimetri aniqlansin.
+// ---------------------------------------------------------------------
+// let a = 5;
+// let b = 3;
+// let y = a * b;
+// let p = 2 * (a + b);
+// console.log(`${y} yuzasi ${p} peremetri`);
+
+// 9.Aylananing diametri d berilgan. Uning uzunligi aniqlansin L = л·а ̧ л=3.14
+// ---------------------------------------------------------------------
+// let d = 10;
+// let p = 3.14;
+// let l = p * d;
+// console.log(l);
+
+// 10.Kvadratning tomoni a berilgan. Uning yuzasi va perimetri aniqlansin.
+// ---------------------------------------------------------------------
+// let a = 5;
+// let y = a * a;
+// let p = 4 * a;
+// console.log(`yuzasi ${y} perimetri ${p}`);
